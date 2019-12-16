bd3372c42be54c5e5df1888332f9e00d08a4af8e
chencheng <591626314@qq.com>
2019-12-16 16:28:25 +0800

    version 3.0 卷积层实现

--- Modules/LeNet.py
+++ Modules/LeNet.py
@@ -1,64 +1,70 @@
+import nn
+import numpy as np
+
 class LeNet:
     def __init__(self):
         # conv layer 1
-        self.conv1 = Conv_Module([8, 1, 3, 3], [8], [1, 1], 'SAME')
-        self.relu1 = Relu_Module()
-        self.pool1 = Pool_Module([2, 2])
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding='SAME')
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
         # conv layer 2
-        self.conv2 = Conv_Module([8, 8, 3, 3], [8], [1, 1], 'SAME')
-        self.relu2 = Relu_Module()
-        self.pool2 = Pool_Module([2, 2])
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, stride=1, padding='SAME')
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        self.linear1 = Linear_Module([7 * 7 * 8, 32], [32])
-        self.relu3 = Relu_Module()
-        self.linear2 = Linear_Module([32, 10], [10])
-        self.ce = CE_Module()
-        self.softmax = Softmax_Module()
+        self.linear1 = nn.Linear(in_features=7 * 7 * 8, out_features=32)
+        self.relu3 = nn.ReLU()
+        self.linear2 = nn.Linear(in_features=32, out_features=10)
+        self.softmax = nn.Softmax()
+        self.ce = nn.CrossEntropyLoss()
+        print('Model_LeNet constructed!')
 
     # Train process
     def train(self, x, y_, learning_rate):
         # forward
-        f1 = self.conv1.forward_pass(x)
-        f2 = self.relu1.forward_pass(f1)
-        f3, max_idx1 = self.pool1.forward_pass(f2)
-        f4 = self.conv2.forward_pass(f3)
-        f5 = self.relu2.forward_pass(f4)
-        f6, max_idx2 = self.pool2.forward_pass(f5)
+        f1 = self.conv1.forward(x)
+        f2 = self.relu1.forward(f1)
+        f3, max_idx1 = self.pool1.forward(f2)
+        f4 = self.conv2.forward(f3)
+        f5 = self.relu2.forward(f4)
+        f6, max_idx2 = self.pool2.forward(f5)
         flat = np.reshape(f6, [-1, 7 * 7 * 8])  # flatten
-        f7 = self.linear1.forward_pass(flat)
-        f8 = self.relu3.forward_pass(f7)
-        f9 = self.linear2.forward_pass(f8)
-        #     f10 = self.ce.forward_pass(f9,y_)
-        #     print(f10)
+        f7 = self.linear1.forward(flat)
+        f8 = self.relu3.forward(f7)
+        f9 = self.linear2.forward(f8)
+        f10 = self.softmax.forward(f9)
+
+        predicted = np.argmax(f10, 1)  # 算出模型输出
+        loss = self.ce.forward(f10, y_)
 
         # backward
-        b1 = self.ce.backward_pass(f9, y_)
-        b2 = self.linear2.backward_pass(b1, f8, learning_rate)
-        b3 = self.relu3.backward_pass(b2, f8)
-        b4 = self.linear1.backward_pass(b3, flat, learning_rate)
+        b1 = self.ce.backward(f10, y_)
+        b2 = self.linear2.backward(b1, f8, learning_rate)
+        b3 = self.relu3.backward(b2, f8)
+        b4 = self.linear1.backward(b3, flat, learning_rate)
         deflat = np.reshape(b4, [-1, 8, 7, 7])  # de-flatten
-        b5 = self.pool2.backward_pass(deflat, max_idx2)
-        b6 = self.relu2.backward_pass(b5, f5)
-        b7 = self.conv2.backward_pass(b6, f3, learning_rate)
-        b8 = self.pool1.backward_pass(b7, max_idx1)
-        b9 = self.relu1.backward_pass(b8, f2)
-        b10 = self.conv1.backward_pass(b9, x, learning_rate)
+        b5 = self.pool2.backward(deflat, max_idx2)
+        b6 = self.relu2.backward(b5, f5)
+        b7 = self.conv2.backward(b6, f3, learning_rate)
+        b8 = self.pool1.backward(b7, max_idx1)
+        b9 = self.relu1.backward(b8, f2)
+        b10 = self.conv1.backward(b9, x, learning_rate)
 
-        return
+        return loss, predicted
 
     def eval(self, x, y_):
         # forward
-        f1 = self.conv1.forward_pass(x)
-        f2 = self.relu1.forward_pass(f1)
-        f3, max_idx1 = self.pool1.forward_pass(f2)
-        f4 = self.conv2.forward_pass(f3)
-        f5 = self.relu2.forward_pass(f4)
-        f6, max_idx2 = self.pool2.forward_pass(f5)
+        f1 = self.conv1.forward(x)
+        f2 = self.relu1.forward(f1)
+        f3, max_idx1 = self.pool1.forward(f2)
+        f4 = self.conv2.forward(f3)
+        f5 = self.relu2.forward(f4)
+        f6, max_idx2 = self.pool2.forward(f5)
         flat = np.reshape(f6, [-1, 7 * 7 * 8])  # flatten
-        f7 = self.linear1.forward_pass(flat)
-        f8 = self.relu3.forward_pass(f7)
-        f9 = self.linear2.forward_pass(f8)
-        y = self.softmax.forward_pass(f9)
+        f7 = self.linear1.forward(flat)
+        f8 = self.relu3.forward(f7)
+        f9 = self.linear2.forward(f8)
+        y = self.softmax.forward(f9)
         # Evaluation
         correct_prediction = np.equal(np.argmax(y_, 1), np.argmax(y, 1))
         accuracy = np.mean(np.array(correct_prediction, dtype=np.float32))
--- nn/pooling.py
+++ nn/pooling.py
@@ -1,11 +1,50 @@
+import numpy as np
+
 # 定义池化层
 class MaxPool2d:
-    def __init__(self, pool_size):
-        print('MaxPool2d constructed')
-        self.ps = pool_size
+    def __init__(self, kernel_size):
+        self.kernel_size = kernel_size
 
-    def forward_pass(self, x):
-        pass
+    def forward(self, x):
+        (samples, cx, hx, wx) = x.shape
+        hy = int(hx / self.kernel_size)
+        wy = int(wx / self.kernel_size)
+        y = np.zeros([samples, cx, hy, wy])
+        x_reshaped = np.reshape(x, (samples * cx * hx, wx))
 
-    def backward_pass(self, dL_dy, max_idx):
-        pass
+        # change x_reshaped to rows*4 matrix
+        for j1 in range(self.kernel_size):
+            for j2 in range(self.kernel_size):
+                tmp = x_reshaped[j1::self.kernel_size, j2::self.kernel_size]
+                col = np.reshape(tmp, (-1, 1))
+                if (j1 == 0 and j2 == 0):
+                    x_col = col
+                else:
+                    x_col = np.concatenate((x_col, col), axis=1)
+
+        max_idx = np.argmax(x_col, axis=1)
+        y = x_col[range(len(max_idx)), max_idx]
+        y = np.reshape(y, (samples, cx, hy, -1))
+        return y, max_idx
+
+    def backward(self, dL_dy, max_idx):
+        (samples, cy, hy, wy) = dL_dy.shape
+        dy_reshaped = np.reshape(dL_dy, (-1, 1))
+        hx = int(hy * self.kernel_size)
+        wx = int(wy * self.kernel_size)
+
+        dy_reshaped = np.transpose(dy_reshaped, (1, 0))
+        dx_reshaped = np.zeros([self.kernel_size * self.kernel_size, len(max_idx)])
+        dx_reshaped[max_idx, range(len(max_idx))] = dy_reshaped
+        dx_reshaped = np.transpose(dx_reshaped, (1, 0))
+
+        img = np.zeros([samples * cy * hx, wx])
+        # change dx_reshaped to a 4-dim matrix dL_dx
+        for j1 in range(self.kernel_size):
+            for j2 in range(self.kernel_size):
+                col = dx_reshaped[:, j1 * self.kernel_size + j2]
+                tmp = img[j1::self.kernel_size, j2::self.kernel_size]
+                img[j1::self.kernel_size, j2::self.kernel_size] = np.reshape(col, (tmp.shape))
+
+        dL_dx = np.reshape(img, (samples, cy, hx, wx))
+        return dL_dx
